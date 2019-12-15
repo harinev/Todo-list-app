43d327e43a55669021549e25b60135e20ad085b5
harine <harinev@outlook.com>
2019-12-15 20:37:20 +0000

    using css for status of count box

--- src/Component/Consolidate.js
+++ src/Component/Consolidate.js
@@ -4,7 +4,7 @@ import './Consolidate.css';
 function Consolidate() {
     return (
         <div className="form-row content" >
-            <div className="col-4 content">
+            <div className="col-4">
                 <p>Total Task</p>
             </div>
             <div className="col-3">
